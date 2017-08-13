be673fe6ed58d78c8aaa07c18afcd26a39f1cbd6
shuai <shuai@192.168.56.1>
2017-08-13 12:45:01 +0800

    add generate car

--- mongodb/src/main/java/com/mongo/MongoTest.java
+++ mongodb/src/main/java/com/mongo/MongoTest.java
@@ -1,19 +1,35 @@
 package com.mongo;
 
 
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+
 import org.bson.Document;
 
+import com.mongo.CarGenerator.Car;
 import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
 public class MongoTest {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		MongoClient client = new MongoClient("192.168.1.107", 27017);
 		MongoDatabase db = client.getDatabase("auction");  
 	    System.out.println("Connect to database successfully");
-	    db.createCollection("user");
-	    for(int i = 0; i < 10000; i++){
-	    	Document document = new Document("id", i);
+	    //db.createCollection("user");
+	    MongoCollection<Document> collection = db.getCollection("user");
+		BeanInfo beanInfo = Introspector.getBeanInfo(Car.class);
+		PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+	    for(int i = 0; i < 500000; i++){
+	    	Car car = CarGenerator.Generated(i);
+	    	Document document = new Document();
+	    	for(PropertyDescriptor descriptor : propertyDescriptors){
+	    		if(descriptor.getName().equals("class")) continue;
+	    		document.append(descriptor.getName(), descriptor.getReadMethod().invoke(car));
+	    	}
+	    	System.out.println(document);
+	    	collection.insertOne(document);
 	    }
 
 	}
